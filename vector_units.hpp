7b2f01851d74ae65fb6982e48f98660e7b9e0f14
codectile <codectile@gmail.com>
2019-04-07 17:34:59 -0600

    Update vector_units.hpp

--- vector_units.hpp
+++ vector_units.hpp
@@ -3,6 +3,8 @@
 
 #include <type_traits>
 #include <boost/static_assert.hpp>
+#include <boost/units/systems/si.hpp>
+#include <boost/units/quantity.hpp>
 
 namespace boost_test
 {
@@ -22,15 +24,12 @@ namespace boost_test
 		BOOST_STATIC_ASSERT_MSG(std::is_same_v<typename U1::dimension_type, typename U2::dimension_type>, "vector_units: all units must be of same dimension");
 		BOOST_STATIC_ASSERT_MSG(std::is_same_v<typename U2::dimension_type, typename U3::dimension_type>, "vector_units: all units must be of same dimension");
 	public:
-
 		/*
 		 * default constructor
 		 */
 		vector_units()
 		{
-			_vector[0] = Type(0);
-			_vector[1] = Type(0);
-			_vector[2] = Type(0);
+			
 		}
 
 		/*
@@ -40,40 +39,89 @@ namespace boost_test
 		 * y - for storing value of y - coordinate
 		 * z - for storing value of z - coordinate
 		 */
-		vector_units(Type x, Type y, Type z)
+		vector_units(const Type& x1, const Type& y1, const Type& z1)
 		{
-			_vector[0] = x;
-			_vector[1] = y;
-			_vector[2] = z;
+			x = boost::units::quantity<U1, Type>::from_value(x1);
+			y = boost::units::quantity<U2, Type>::from_value(y1);
+			z = boost::units::quantity<U3, Type>::from_value(z1);
+		}
+
+		/*
+		 * parameterized constructor for quantity type
+		 * Parameters:
+		 * x - for storing value of x - coordinate
+		 * y - for storing value of y - coordinate
+		 * z - for storing value of z - coordinate
+		 */
+		vector_units(boost::units::quantity<U1, Type> x1, boost::units::quantity<U2, Type> y1, boost::units::quantity<U3, Type> z1)
+		{
+			x = x1;
+			y = y1;
+			z = z1;
 		}
 
 		/*
 		 * stores the value for x-ccordinate
-		 * Template parameters:
-		 * N - 0 for x, 1 for y and 2 for z coordinate
 		 * Function parameters:
 		 * value - value to be stored
 		 * Return type: void
 		 */
-		template<const size_t N>
-		inline void set(Type value)
+		inline void setx(boost::units::quantity<U1, Type> x1)
 		{
-			BOOST_STATIC_ASSERT_MSG(N < 3, "vector_units: invalid dimension");
-			_vector[N] = value;
+			x = x1;
 		}
 
 		/*
-		 * returns the value for x-ccordinate
-		 * Template parameters:
-		 * N - 0 for x, 1 for y and 2 for z coordinate
+		 * stores the value for y-ccordinate
 		 * Function parameters:
-		 * Return type: Type
+		 * value - value to be stored
+		 * Return type: void
 		 */
-		template<const size_t N>
-		inline Type get() const
+		inline void sety(boost::units::quantity<U2, Type> y1)
 		{
-			BOOST_STATIC_ASSERT_MSG(N < 3, "vector_units: invalid dimension");
-			return _vector[N];
+			y = y1;
+		}
+
+		/*
+		 * stores the value for x-ccordinate
+		 * Function parameters:
+		 * value - value to be stored
+		 * Return type: void
+		 */
+		inline void setz(boost::units::quantity<U3, Type> z1)
+		{
+			z = z1;
+		}
+
+
+		/*
+		 * returns the value for x-ccordinate
+		 * Function parameters
+		 * Return type: quantity<U1, Type>
+		 */
+		inline boost::units::quantity<U1, Type> getx() const
+		{
+			return x;
+		}
+
+		/*
+		 * returns the value for y-ccordinate
+		 * Function parameters
+		 * Return type: quantity<U2, Type>
+		 */
+		inline boost::units::quantity<U2, Type> gety() const
+		{
+			return y;
+		}
+
+		/*
+		 * returns the value for z-ccordinate
+		 * Function parameters
+		 * Return type: quantity<U3, Type>
+		 */
+		inline boost::units::quantity<U3, Type> getz() const
+		{
+			return z;
 		}
 
 		/*
@@ -83,27 +131,14 @@ namespace boost_test
 		 * v - another vector of type vector units
 		 * Return type: vector_units
 		 */
-		 //template<typename T1, typename T2, typename T3, typename T4>
-		inline vector_units cross(const vector_units &v) const
+		vector_units cross(const vector_units &v) const
 		{
 
-			vector_units  up;
-			up._vector[0] = (_vector[1] * v._vector[2] - _vector[2] * v._vector[1]);
-			up._vector[1] = (-(_vector[0] * v._vector[2] - _vector[2] * v._vector[0]));
-			up._vector[2] = (_vector[0] * v._vector[1] - _vector[1] * v._vector[0]);
+			auto x1 = (static_cast<boost::units::quantity<U1, Type>>(y) * static_cast<boost::units::quantity<U1, Type>>(v.getz()) - static_cast<boost::units::quantity<U1, Type>>(z) * static_cast<boost::units::quantity<U1, Type>>(v.gety()));
+			auto y1 = (-(static_cast<boost::units::quantity<U2, Type>>(x) * static_cast<boost::units::quantity<U2, Type>>(v.getz()) - static_cast<boost::units::quantity<U2, Type>>(z) * static_cast<boost::units::quantity<U2, Type>>(v.getx())));
+			auto z1 = (static_cast<boost::units::quantity<U2, Type>>(x) * static_cast<boost::units::quantity<U2, Type>>(v.gety()) - static_cast<boost::units::quantity<U2, Type>>(y) * static_cast<boost::units::quantity<U2, Type>>(v.getx()));
+			vector_units  up(x1.value(), y1.value(), z1.value());
 			return up;
-		}
-
-		/*
-		 * returns the dotproduct of 2 vectors with same units and dimensions
-		 * Template parameters:
-		 * Function parameters:
-		 * v - another vector of type vector_units
-		 * Return type: Type
-		 */
-		inline Type dot(const vector_units<Type, U1, U2, U3> &v) const
-		{
-			return (_vector[0] * v._vector[0] + _vector[1] * v._vector[1] + _vector[2] * v._vector[2]);
 		}
 
 		/*
@@ -116,43 +151,12 @@ namespace boost_test
 		inline vector_units<Type, U1, U2, U3> sum(const vector_units<Type, U1, U2, U3> &v)
 		{
 			vector_units sum;
-			sum._vector[0] = v._vector[0];
-			sum._vector[1] = v._vector[1];
-			sum._vector[2] = v._vector[2];
+			sum.setx(x + v.getx());
+			sum.sety(y + v.gety());
+			sum.setz(z + v.getz());
 			return sum;
 		}
 
-		/*
-		 * multiplies a scalar quantity to this vector_units
-		 * Template parameters:
-		 * Function parameters:
-		 * x - scalar value of datatype Type
-		 * Return type: vector_units
-		 */
-		vector_units<Type, U1, U2, U3> operator* (Type x)
-		{
-			vector_units product;
-			product._vector[0] *= x;
-			product._vector[1] *= x;
-			product._vector[2] *= x;
-			return product;
-		}
-
-		/*
-		 * divides a scalar quantity to this vector_units
-		 * Template parameters:
-		 * Function parameters:
-		 * x - scalar value of datatype Type
-		 * Return type: vector_units
-		 */
-		vector_units<Type, U1, U2, U3> operator/ (Type x)
-		{
-			vector_units div;
-			div._vector[0] /= x;
-			div._vector[1] /= x;
-			div._vector[2] /= x;
-			return div;
-		}
 
 		/*
 		 * assigns another vector_units to this vector_units
@@ -165,121 +169,19 @@ namespace boost_test
 		{
 			if (this != &v)
 			{
-				_vector[0] = v._vector[0];
-				_vector[1] = v._vector[1];
-				_vector[2] = v._vector[2];
+				x = v.getx();
+				y = v.gety();
+				z = v.getz();
 			}
 			return *this;
 		}
 
-		/*
-		 * overloaded subscript operator
-		 * Template parameters:
-		 * Function parameters:
-		 * i - index for the vector
-		 * Return type: Type refernce
-		 */
-		inline Type &operator[](const size_t i)
-		{
-			return _vector[i];
-		}
-
-		/*
-		 * overloaded subscript operator
-		 * Template parameters:
-		 * Function parameters:
-		 * i - index for the vector
-		 * Return type: Type
-		 */
-		inline Type operator[](const size_t i) const
-		{
-			return _vector[i];
-		}
 
 	private:
-		Type _vector[3];
+		boost::units::quantity<U1, Type> x;
+		boost::units::quantity<U2, Type> y;
+		boost::units::quantity<U3, Type> z;
+		
 	};
-
-
-
-
-
-	//expression template for addition of vectors
-	template<typename Type, typename U1, typename U2, typename U3>
-	inline vector_units<Type, U1, U2, U3> operator+(const vector_units<Type, U1, U2, U3> &u, const vector_units<Type, U1, U2, U3> &v)
-	{
-		vector_units< Type, U1, U2, U3> sum;
-		for (size_t i = 0; i < 3; i++)
-			sum[i] = u[i] + v[i];
-		return sum;
-	}
-
-	template<typename Type, typename U1, typename U2, typename U3>
-	inline vector_units<Type, U1, U2, U3> operator-(const vector_units<Type, U1, U2, U3> &u, const vector_units<Type, U1, U2, U3> &v)
-	{
-		vector_units<Type, U1, U2, U3> diff;
-		for (size_t i = 0; i < 3; i++)
-			diff[i] = u[i] - v[i];
-		return diff;
-	}
-
-
-	template <typename Type, typename E>
-	class vector_units_expr
-	{
-	public:
-		inline Type operator[](const size_t i) const
-		{
-			return static_cast<const E &>(*this)[i];
-		}
-	};
-
-	template <typename Type, typename E1, typename E2>
-	class vector_units_sum : public vector_units_expr<Type, vector_units_sum<Type, E1, E2>>
-	{
-	private:
-		const E1 &_u;
-		const E2 &_v;
-
-	public:
-		vector_units_sum(const E1 &u, const E2 &v) : _u(u), _v(v)
-		{
-		}
-
-		inline Type operator[](const size_t i) const
-		{
-			return _u[i] + _v[i];
-		}
-	};
-
-	template <typename Type, typename E1, typename E2>
-	class vector_units_diff : public vector_units_expr<Type, vector_units_sum<Type, E1, E2>>
-	{
-	private:
-		const E1 &_u;
-		const E2 &_v;
-
-	public:
-		vector_units_diff(const E1 &u, const E2 &v) : _u(u), _v(v)
-		{
-		}
-
-		inline Type operator[](const size_t i) const
-		{
-			return _u[i] - _v[i];
-		}
-	};
-
-	template <typename Type, typename E1, typename E2>
-	inline vector_units_sum<Type, E1, E2> operator+(const E1 &u, const E2 &v)
-	{
-		return vector_units_sum<Type, E1, E2>(u, v);
-	}
-
-	template <typename Type, typename E1, typename E2>
-	inline vector_units_diff<Type, E1, E2> operator-(const E1 &u, const E2 &v)
-	{
-		return vector_units_sum<Type, E1, E2>(u, v);
-	}
 }
 #endif // VECTOR_UNITS_HPP
